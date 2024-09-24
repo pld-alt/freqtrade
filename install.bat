ee13ad5ad42a39526c928e5874a24cf18192e9f2
pld-alt <pld65487@gmail.com>
2024-09-24 18:12:07 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+pip install --find-links build_helpers\ TA-Lib -U
+pip install -r requirements.txt
+pip install -e .
+freqtrade install-ui
+freqtrade create-userdir --userdir "%USERPROFILE%\FreqTrade"
+freqtrade new-config --config "%USERPROFILE%\FreqTrade\config.json"
